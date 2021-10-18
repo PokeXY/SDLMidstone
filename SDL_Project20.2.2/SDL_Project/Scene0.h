f9b3de0040ba3990fc8c00f1be1a6e99ea9edad0
Garen Johnstone <garennj@gmail.com>
2021-10-17 20:13:49 -0400

    remove unnecessary getProjectionMatrix

--- SDL_Project20.2.2/SDL_Project/Scene0.h
+++ SDL_Project20.2.2/SDL_Project/Scene0.h
@@ -32,8 +32,6 @@ public:
 	void Update(const float time);
 	void Render();
 	void HandleEvents(const SDL_Event &sdlEvent );
-
-	Matrix4 getProjectionMatrix() { return projectionMatrix; }
 };
 
 #endif
