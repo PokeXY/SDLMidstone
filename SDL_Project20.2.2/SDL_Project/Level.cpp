8776e51ebf208b564fa125f966858b1bc5a6ccda
Lamster1111 <leomacrury@gmail.com>
2021-11-20 13:06:06 -0500

    Added a new level layout

--- SDL_Project20.2.2/SDL_Project/Level.cpp
+++ SDL_Project20.2.2/SDL_Project/Level.cpp
@@ -88,8 +88,8 @@ void Level::makeLevel(int sceneNum) { //Makes the level by placing all the walls
 		break;
 
 
-	case 1: // ## walls needed in scene1
-				//Surronding walls plus a hole for a door
+	case 1: // 78 walls needed in scene1 for a upside down pyramid
+		//Surronding walls plus a hole for a door
 		//Xpos1 is supose to go here
 		for (int i = 0; i < 16; ++i) { //Top line of walls 16 wall needed
 			walls[i]->setPos(Vec3(xpos1, 17.0f, 0.0f));
@@ -145,38 +145,54 @@ void Level::makeLevel(int sceneNum) { //Makes the level by placing all the walls
 
 		break;
 
-	case 2:
+	case 2: // Circle thing requari 58 walls
 		//Surronding walls plus a hole for a door
-//Xpos1 is supose to go here
+		//Xpos1 is supose to go here
 		for (int i = 0; i < 16; ++i) { //Top line of walls 16 wall needed
-			walls[i]->setPos(Vec3(xpos1, 18.0f, 0.0f));
+			walls[i]->setPos(Vec3(xpos1, 17.0f, 0.0f));
 			xpos1 += 2;
 		}
 
 		for (int i = 16; i < 24; ++i) { //Left line of walls 8 walls needed
-			walls[i]->setPos(Vec3(0.0f, ypos1, 0.0f));
+			walls[i]->setPos(Vec3(1.0f, ypos1, 0.0f));
 			ypos1 += 2;
 		}
 
 		for (int i = 24; i < 40; ++i) { //Bottom line of walls 15 wall needed
-			walls[i]->setPos(Vec3(xpos2, 2.0f, 0.0f));
+			walls[i]->setPos(Vec3(xpos2, 1.0f, 0.0f));
 			xpos2 += 2;
 		}
 
 		for (int i = 40; i < 43; ++i) { //Right bottom under door 3 walls needed
-			walls[i]->setPos(Vec3(30.0f, ypos2, 0.0f));
+			walls[i]->setPos(Vec3(31.0f, ypos2, 0.0f));
 			ypos2 += 2;
 		}
 
 		ypos2 += 2;
 		for (int i = 43; i < 46; ++i) { //Right upper under door 3 walls needed
-			walls[i]->setPos(Vec3(30.0f, ypos2, 0.0f));
+			walls[i]->setPos(Vec3(31.0f, ypos2, 0.0f));
 			ypos2 += 2;
-
 		}
 
-		//Interior walls
+		//Interior walls the circle bit I could write a loop but this is easier 
 
+		walls[46]->setPos(Vec3(15.0f, 3.0f, 0.0f));
+		walls[47]->setPos(Vec3(17.0f, 3.0f, 0.0f));
+									 
+		walls[48]->setPos(Vec3(13.0f, 5.0f, 0.0f));
+		walls[49]->setPos(Vec3(19.0f, 5.0f, 0.0f));
+									  
+		walls[50]->setPos(Vec3(11.0f, 7.0f, 0.0f));
+		walls[51]->setPos(Vec3(21.0f, 7.0f, 0.0f));
+									  
+		walls[52]->setPos(Vec3(11.0f, 11.0f, 0.0f));
+		walls[53]->setPos(Vec3(21.0f, 11.0f, 0.0f));
+									  
+		walls[54]->setPos(Vec3(13.0f, 13.0f, 0.0f));
+		walls[55]->setPos(Vec3(19.0f, 13.0f, 0.0f));
+									  
+		walls[56]->setPos(Vec3(15.0f, 15.0f, 0.0f));
+		walls[57]->setPos(Vec3(17.0f, 15.0f, 0.0f));
 
 		break;
 
